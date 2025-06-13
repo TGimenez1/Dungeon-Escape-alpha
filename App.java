b6b2cb3f8f37ee167e9475b87c8bbce48b2ac495
Thomas Gimenez <thomas.gimenez31@gmail.com>
2025-06-13 11:13:19 +0200

    Added methods add, multiply, isequal, isgreater

--- App.java
+++ App.java
@@ -10,7 +10,16 @@ public class App {
         System.out.println(f3.toString() + " ou " + f3.toDouble());
         Fraction f4 = new Fraction(-6,3);
         System.out.println(f4.toString() + " ou " + f4.toDouble());
-        Fraction f5 = new Fraction(-2,6);
+        Fraction f5 = new Fraction(2,-6);
         System.out.println(f5.toString() + " ou " + f5.toDouble());
+        System.out.println(f1.multiply(f3).toString()); // 7/10
+        System.out.println(f1.add(f3).toString()); // 19/10
+        System.out.println(f4.multiply(f5).toString()); // 2/3
+        System.out.println(f4.add(f5).toString()); // -7/3
+        Fraction f6 = new Fraction(-5,-10);
+        System.out.println(f1.isequal((f5)));
+        System.out.println(f1.isequal((f6)));
+        System.out.println(f4.isgreater((f3)));
+        System.out.println(f3.isgreater((f1)));
    }
 }
--- maths/Fraction.java
+++ maths/Fraction.java
@@ -7,22 +7,42 @@ public class Fraction {
     public Fraction(int num, int den) {
         this.num = num;
         this.den = den;
-        int newnum = MathUtils.pgcd(this.num,this.den);
-        newnum = Math.abs(newnum); //Comme ça le dénominateur sera toujours positif
-        this.num /= newnum;
-        this.den /= newnum;
+        this.simplify();
+        // Une fraction ne peut exister que sous forme simplifiée
         if (this.den <= 0) {
             throw new IllegalArgumentException("Le dénominateur doit être strictement positif");
         }
     }
 
-/*     public Fraction add(Fraction other) {
+    public Fraction simplify() {
+        if (den < 0) {
+            this.num *= -1;
+            this.den *= -1;
+        }
+        int newnum = MathUtils.pgcd(this.num,this.den);
+        newnum = Math.abs(newnum); //Comme ça le dénominateur sera toujours positif
+        this.num /= newnum;
+        this.den /= newnum;
+        return this;
+    }
 
+    public Fraction add(Fraction other) {
+        return new Fraction(this.num * other.den + other.num * this.den, this.den * other.den);
     }
 
     public Fraction multiply(Fraction other) {
-        
-    } */
+        return new Fraction(this.num * other.num, this.den * other.den);
+    }
+
+    public boolean isequal(Fraction other) {
+        return (this.num == other.num && this.den == other.den);
+    }
+
+    public boolean isgreater(Fraction other) {
+        //Strictly
+        return this.toDouble() > other.toDouble();
+    }
+
     public double toDouble() {
         return (double)this.num / this.den;
     }
@@ -30,6 +50,5 @@ public class Fraction {
     public String toString() {
         return this.num + "/" + this.den;
     }
-
 
 }
